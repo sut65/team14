9112f3a54d7836b2e1d369a81b9155127ca10590
Tepph4 <Teppharit423@gmail.com>
2023-02-06 18:03:26 +0700

    Del Order UI - close #109

--- frontend/src/components/Order_food.tsx
+++ frontend/src/components/Order_food.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import MuiInput from '@mui/material/Input';
+import SearchIcon from '@mui/icons-material/Search';
 import { 
   DeleteOrder,
     ListOrders,ListOrderByBookingCode
@@ -18,33 +19,92 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
+import MuiAlert, { AlertProps } from "@mui/material/Alert";
 import Paper from '@mui/material/Paper';
+import TextField from "@mui/material/TextField";
+import { Grid, IconButton, InputBase, Snackbar, styled } from "@mui/material";
 
-
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(
+  props, ref
+) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+const Input = styled(MuiInput)`
+  width: 42px;
+`;
 function Order_food() {
 
-    const [order_foods, setOrder_foods] = React.useState<Order_foodInterface[]>([]);    
-    const [success, setSuccess] = React.useState(false);
-    const [error, setError] = React.useState(false);
-    const [errorMessage, setErrorMessage] = React.useState("");
-    
-    const listOrder_foods = async () => {
-          let res = await ListOrders();
-          if (res != null) {
-            setOrder_foods(res);
-          }
-    };   
-    const listOrdersBybookingCode = async () => {
-      let res = await ListOrders();
-      if (res != null) {
-        setOrder_foods(res);
+    const [order_foods, setOrder_foods] = useState<Order_foodInterface[]>([]);
+
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+
+    const [successDel, setSuccessDel] = useState(false);
+    const [errorDel, setErrorDel] = useState(false);
+
+    const [errorMessageDel, setErrorMessageDel] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+    const [code, setCode] = useState(""); 
+    const [errorSearch, setErrorSearch] = useState(false); 
+    const [ OrderID, setOrderID] = useState("");
+  
+
+    const handleClose = (
+      event?: React.SyntheticEvent | Event,
+      reason?: string
+    ) => {
+      if (reason === "clickaway") {
+          return;
       }
-};  
+      setSuccess(false);
+      setError(false);
 
-    useEffect(() => {
-        listOrder_foods();        
-      }, []);
+      setSuccessDel(false);
+      setErrorDel(false);
+     
+ };
+  async function search(){
+    if (code === ""){
+      setErrorSearch(true);
+      setErrorMessage("กรุณากรอกรหัสการจองห้องที่จะค้นหา");
+      return
+    }  
+    let res = await ListOrderByBookingCode(code);  
+    if (res != null){
+      setOrder_foods(res.data);   
+      handleClose()
+      setSuccess(true);
+      setErrorMessage("");
+      console.log(res.data);
+      } else {
+        setErrorSearch(true);
+        setErrorMessage(res);
+      }
+  };
 
+  const DelOrder  = async (id:any) => {
+    let res = await DeleteOrder(id);
+    if (res != null) {
+      let res = await ListOrderByBookingCode(code);
+      if (res != null){
+        setOrder_foods(res.data);   
+        handleClose()
+        setSuccessDel(true);
+        setErrorMessage("");
+        console.log(res.data);
+        console.log(res.data);
+        } else {
+          setErrorSearch(true);
+          setErrorMessage(res);
+        }     
+      
+    }
+    else{
+      setErrorDel(true);
+      setErrorMessageDel(res);
+    }
+}; 
+       
     
  return (
     <div>
@@ -55,36 +115,83 @@ function Order_food() {
            marginTop: 2,
          }}
        >
+      <Snackbar
+              open={success}
+              autoHideDuration={6000}
+              onClose={handleClose}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+            >
+              <Alert onClose={handleClose} severity="success">
+                ค้นหาข้อมูลสำเร็จ
+              </Alert>
+      </Snackbar>
+      <Snackbar open={errorSearch} autoHideDuration={6000} onClose={handleClose}>
+          <Alert onClose={handleClose} severity="error">
+            ค้นหาข้อมูลไม่สำเร็จ: {errorMessage}
+          </Alert>
+      </Snackbar>
 
+      <Snackbar
+       open={successDel}
+       autoHideDuration={6000}
+       onClose={handleClose}
+       anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+     >
+       <Alert onClose={handleClose} severity="success">
+         ยกเลิกรายการสั่งสำเร็จ
+       </Alert>
+     </Snackbar>
+     <Snackbar open={error} autoHideDuration={6000} onClose={handleClose}>
+       <Alert onClose={handleClose} severity="error">
+         ยกเลิกรายการสั่งไม่สำเร็จ: {errorMessageDel}
+       </Alert>
+     </Snackbar>
+     
          <Box flexGrow={1}>
-           <Typography
-             component="h2"
+           <Typography             
              variant="h6"
              color="primary"
              gutterBottom
+             component="h4"
            >
              จัดการร้องขออาหารและเครื่องดื่ม
            </Typography>
          </Box>
          
-
          <Box>
          <ButtonGroup variant="contained" aria-label="Disabled elevation buttons">
             <Button component={RouterLink}
                     to="/order_food/create"
                     variant="contained"
                     color="primary" > Create Order </Button>
-            <Button component={RouterLink}
-                    to="/order_food/update"
+            <Button 
                     variant="outlined"
-                    color="primary" >Edit</Button>
-            <Button component={RouterLink}
-                    to="/order_food/update"
-                    variant="outlined"
-                    color="error">Del</Button>
+                    color="primary" >Edit</Button>           
         </ButtonGroup>           
          </Box>
        </Box>
+       <Paper
+            component="form"
+            sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}
+          >      
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="Booking Code"
+            inputProps={{ 'aria-label': 'Booking Code' }}
+            value={code + ""}
+                  onChange={(e) => {setCode(e.target.value)                              
+                    console.log(code)
+                    }
+                  }
+          />
+            <IconButton type="button" sx={{ p: '10px' }} 
+                        aria-label="search" size="large"
+                        onClick= {() => {
+                        search();
+                        }}>
+              <SearchIcon />
+            </IconButton>      
+        </Paper> 
 
        <div style={{ height: 400, width: "100%", marginTop: '20px'}}>
        <TableContainer component={Paper}>
@@ -119,10 +226,14 @@ function Order_food() {
                                           aria-label="Disabled elevation buttons"
                                           size="small"
                                         >
-                                          <Button variant="contained">Edit</Button>
-                                          <Button variant="outlined" onClick={() => DeleteOrder(row.ID)}>Del</Button>
+                                          <Button variant="contained" 
+                                                  component={RouterLink}
+                                                  to="/order_food/update" >Edit</Button>
+                                          <Button variant="outlined" 
+                                                  color="error" 
+                                                  onClick={() => DelOrder(row.ID)}>Del</Button>
                                         </ButtonGroup>
-              </TableCell>
+              </TableCell>  
             </TableRow>
           ))}
         </TableBody>
