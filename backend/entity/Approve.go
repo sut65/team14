517727a005bf356ac2f123435595c8fd62512350
MixGaming0959 <wasutew@gmail.com>
2023-02-14 23:46:31 +0700

    แก้ validation

--- backend/entity/Approve.go
+++ backend/entity/Approve.go
@@ -32,37 +32,39 @@ type Approve struct {
 }
 
 func init() {
-	govalidator.CustomTypeTagMap.Set("IsFuture", func(i interface{}, context interface{}) bool {
+	govalidator.CustomTypeTagMap.Set("IsFuture", func(i interface{}, _ interface{}) bool {
 		t := i.(time.Time)
 		return t.After(time.Now())
 	})
 
-	govalidator.CustomTypeTagMap.Set("IsPresent", func(i interface{}, context interface{}) bool {
+	govalidator.CustomTypeTagMap.Set("IsPresent", func(i interface{}, _ interface{}) bool {
 		t := i.(time.Time)
 		return t.After(time.Now().AddDate(0, 0, -1)) && t.Before(time.Now().AddDate(0, 0, 1))
 	})
 
-	govalidator.CustomTypeTagMap.Set("IsPast", func(i interface{}, context interface{}) bool {
+	govalidator.CustomTypeTagMap.Set("IsPast", func(i interface{}, _ interface{}) bool {
 		t := i.(time.Time)
 		return t.Before(time.Now())
 	})
-	govalidator.CustomTypeTagMap.Set("IsnotPast", func(i interface{}, o interface{}) bool {
+	govalidator.CustomTypeTagMap.Set("IsnotPast", func(i interface{}, _ interface{}) bool {
 		t := i.(time.Time)
 		// ย้อนหลังไม่เกิน 1 วัน
 		return t.After(time.Now().AddDate(0, 0, -1))
 	})
-	govalidator.CustomTypeTagMap.Set("IsnotPastAndnotFuture", func(i interface{}, o interface{}) bool {
+	govalidator.CustomTypeTagMap.Set("IsnotPastAndnotFuture", func(i interface{}, _ interface{}) bool {
 		t := i.(time.Time)
 		// +- 1 วัน
 		return t.After(time.Now().AddDate(0, 0, -1)) && t.After(time.Now().AddDate(0, 0, 1))
 	})
-	govalidator.CustomTypeTagMap.Set("DelayNow3Min", func(i interface{}, context interface{}) bool {
+	govalidator.CustomTypeTagMap.Set("DelayNow3Min", func(i interface{}, _ interface{}) bool {
 		t := i.(time.Time)
 		a := t.After(time.Now().Add(-3 * time.Minute))
 		b := t.Before(time.Now().Add(+3 * time.Minute))
 		sts := a && b
-		println(a)
-		println(b)
 		return sts
+	})
+	govalidator.CustomTypeTagMap.Set("IsNotNegativeNumbers", func(i interface{}, _ interface{}) bool {
+		n := i.(float32)
+		return n >= 0
 	})
 }
